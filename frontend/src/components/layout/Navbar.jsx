1edaa80552c678e9e62f546fe99bd3a8d14687bf
b8stianjaja <bastianpuga3@gmail.com>
2025-10-18 14:26:31 -0300

    Navbar Update

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -82,7 +82,7 @@ function Navbar() {
     ));
 
   return (
-    <nav className="bg-white shadow-lg sticky top-0 z-50 border-b border-gray-200">
+    <nav className="bg-white sticky top-0 z-50 border-b-2 border-gray-300">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center h-16">
         <div className="flex-shrink-0">
           <a href="#hero" className="flex items-center"></a>
